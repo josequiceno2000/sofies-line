87d1be7f0ff46fa548865e72cf033d6e95b3734f
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-04-05 10:37:15 -0500

    Finalize core game loop

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from intro import intro 
-from rounds import get_quote, round_result
+from rounds import print_round, get_quote, round_result
 
 def main():
     # Intro
     player_character = intro()
 
     # Rounds
-    philosopher, quote, philosophers = get_quote()
-    user_choice = int(input("\n> "))
-    round_result(player_character, user_choice, quote, philosopher, philosophers)
+    round_number = 1
+
+    while round_number < 6 and player_character.lives > 0:
+        print_round(round_number)
+        philosopher, quote, philosophers = get_quote()
+        user_choice = int(input("\n> "))
+        round_result(player_character, user_choice, quote, philosopher, philosophers)
+        round_number += 1
                 
 
     
