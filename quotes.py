7d300e06c05354a8fc26a2b9b8d41e73720f97ef
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-04-04 22:25:08 -0500

    Quotes added

---
+++ quotes.py
@@ -0,0 +1,36 @@
+philosopher_quotes = {
+    "Socrates": [
+        "The unexamined life is not worth living.",
+        "To know, is to know that you know nothing. That is the meaning of true knowledge.",
+        "Wonder is the beginning of wisdom.",
+        "Let him that would move the world, first move himself."
+        "Strong minds discuss ideas, average minds discuss events, weak minds discuss people."
+    ],
+    "Plato": [
+        "The measure of a man is what he does with power.",
+        "Knowledge is true opinion.",
+        "Reality is merely an illusion, albeit a very persistent one.",
+        "We can easily forgive a child who is afraid of the dark; the real tragedy of life is when men are afraid of the light.",
+        "Music is a moral law. It gives soul to the universe, wings to the mind, flight to the imagination, and charm and gaiety to life and everything."
+    ],
+    "Aristotle": [
+        "We are what we repeatedly do. Excellence, then, is not an act, but a habit.",
+        "Happiness depends upon ourselves.",
+        "The aim of art is to represent not the outward appearance of things, but their inward significance.",
+        "It is the mark of an educated mind to be able to entertain a thought without accepting it.",
+        "Knowing yourself is the beginning of all wisdom."
+    ],
+    "Aquinas": [
+        "To love is to will the good of the other.",
+        "There is nothing on this earth more to be prized than true friendship.",
+        "The things that we love tell us what we are.",
+        "To one who has faith, no explanation is necessary. To one without faith, no explanation is possible.",
+        "Man cannot live without joy; therefore when he is deprived of true spiritual joys, it is necessary that he become addicted to carnal pleasures."
+    ],
+    "Albertus Magnus": [
+        "Every judgment about the truth of anything whatever must be based upon direct observation.",
+        "Experience is the only teacher of those things which fall under the senses.",
+        "The roots of all goodness lie in the soil of appreciation for goodness.",
+        "In studying nature we have to inquire not how nature may be and how we would have it to be, but how nature actually is."
+    ]
+}
